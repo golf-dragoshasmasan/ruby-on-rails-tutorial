38b455e27e565ec839a561354b22a7bfd153b7e5
Hasmasan Dragos <hasmasandragos@Hasmasans-MacBook-Pro.local>
2025-09-25 18:48:50 +0300

    Add basic relationships

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   has_many :microposts, dependent: :destroy
+  has_many :active_relationships, class_name: "Relationship", foreign_key: "follower_id", dependent: :destroy
 
   before_save { email.downcase! }
   validates(:name, presence: true, length: { maximum: 50 })
