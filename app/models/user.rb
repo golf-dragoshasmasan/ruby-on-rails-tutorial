21e2299004eb9356af0e3c719682e177d99ff3dc
Hasmasan Dragos <hasmasandragos@Hasmasans-MacBook-Pro.local>
2025-09-25 14:16:19 +0300

    Add microposts scaffolding

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ApplicationRecord
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  has_many :microposts
 
   before_save { email.downcase! }
   validates(:name, presence: true, length: { maximum: 50 })
