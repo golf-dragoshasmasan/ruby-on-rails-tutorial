1dbb24fdef44fd09c1c72deacd35806db1c6298e
Hasmasan Dragos <hasmasandragos@Hasmasans-MacBook-Pro.local>
2025-09-26 12:30:58 +0300

    Add user following

--- app/models/user.rb
+++ app/models/user.rb
@@ -14,10 +14,10 @@ class User < ApplicationRecord
   has_secure_password
   validates :password, presence: true, length: { minimum: 6 }, allow_nil: true
 
-  # Defines a proto-feed.
-  # See "Following users" for the full implementation.
+  # Returns a user's status feed.
   def feed
-    Micropost.where("user_id = ?", id)
+    following_ids = "SELECT followed_id FROM relationships WHERE follower_id = :user_id"
+    Micropost.where("user_id IN (#{following_ids}) OR user_id = :user_id", user_id: id)
   end
 
   # Follows a user.
